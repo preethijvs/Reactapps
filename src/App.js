0bcbd14b1c6050ed0c639f7cb75bcd4a0da0c2b9
Preethi JVS <130128008+preethijvs@users.noreply.github.com>
2023-07-15 22:05:59 +0530

    Update App.js
    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import New from "./component/New";
 function App() {
-  const fruits = ["apple", "BlueBerry", "mango"];
+  const fruits = ["apple", "BlueBerry", "mangos"];
   const color = ["red", "Blue", "yellow"];
 
   return <New fruits={fruits} color={color} />;
